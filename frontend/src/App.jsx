33fab648543a2a1c3e13964bed37fff117b887da
Saksham1411 <sakshamsamgarg1411@gmail.com>
2024-02-06 19:56:34 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
+
+function App() {
+  return(
+    <div className="p-4 h-screen flex items-center justify-center">
+      {/* <Login/> */}
+      {/* <SignUp/> */}
+      <Home/>
+      
+    </div>
+  )
+}
+
+export default App

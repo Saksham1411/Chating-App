08734498c5dab79f399fcd88ec583be7c87efc35
Saksham1411 <sakshamsamgarg1411@gmail.com>
2024-02-08 16:13:28 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,9 @@ const PORT = process.env.PORT || 4000;
 
 app.use(cookieParser());
 app.use(express.json());
-// app.use(cors({
-//     credentials: true,
-//     // origin: "http://localhost:5173",
-// }));
+app.use(cors({
+    credentials: true,
+}));
 app.use(express.urlencoded());
 
 app.use('/api', userRoutes);
